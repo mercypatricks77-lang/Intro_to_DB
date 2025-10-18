6868cc7a0e937603d901af6bf02fbcf96814f0e7
Ms. Mwangi Mercy <mercypatricks77@gmail.com>
2025-10-19 03:00:53 +0500

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -6,3 +6,5 @@ INSERT INTO CUSTOMERS (CUSTOMER_NAME, EMAIL, ADDRESS)
 VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO CUSTOMERS (CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
