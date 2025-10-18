7f94cb3ab7e827e5072979041f318ca172958e39
Ms. Mwangi Mercy <mercypatricks77@gmail.com>
2025-10-19 02:47:17 +0500

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,8 @@
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
+cat task_4.sql
+git add task_4.sql
+git commit -m "Add task_4.sql to describe books table without DESCRIBE or EXPLAIN"
+git push
+
